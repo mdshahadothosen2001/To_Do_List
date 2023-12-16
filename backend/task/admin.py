fd46ad51ac3697a9e2e95327c17445773c81d21e
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-12-16 18:36:22 +0600

    feat: task model created

---
+++ backend/task/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import TaskModel
+
+class TaskModelAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "title",
+        "description",
+        "complete",
+        "created_at",
+        "updated_at",
+    )
+    list_display_links = (
+        "user",
+        "title",
+    )
+    search_fields = (
+        "user",
+        "title",
+    )
+
+
+admin.site.register(TaskModel, TaskModelAdmin)
