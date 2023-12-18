3159289996360779906ca6f8b657d27c4916bb57
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-12-18 20:04:55 +0600

    feat: each task detail

--- backend/task/urls.py
+++ backend/task/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, TaskDetailView
 
 urlpatterns = [
-    path(route='',view=TaskListView.as_view(), name='tasks'),
+    path(route='', view=TaskListView.as_view(), name='tasks'),
+    path(route="task/<int:pk>/", view=TaskDetailView.as_view(), name='task'),
 ]
--- backend/task/views.py
+++ backend/task/views.py
@@ -1,7 +1,17 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import TaskModel
 
 
 class TaskListView(ListView):
+    """This class for display list of tasks"""
+
     model = TaskModel
     context_object_name = 'tasks'
+
+
+class TaskDetailView(DetailView):
+    """This class for display task details each task"""
+
+    model = TaskModel
+    context_object_name = 'task'
