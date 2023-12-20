8eed141060e083ff513df50684813993b617d01a
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-12-20 07:53:40 +0600

    feat: task update api

--- backend/task/urls.py
+++ backend/task/urls.py
@@ -2,14 +2,17 @@ from django.urls import path
 from .views import (
     TaskListView,
     TaskDetailView,
-    TaskCreateView
+    TaskCreateView,
+    TaskUpdateView
     )
 
 urlpatterns = [
     #http://localhost:8000/task/
     path(route='', view=TaskListView.as_view(), name='tasks'),
-    #http://localhost:8000/task/1
+    #http://localhost:8000/task/1/
     path(route="<int:pk>/", view=TaskDetailView.as_view(), name='task'),
-    #http://localhost:8000/task/create
-    path(route="create/", view=TaskCreateView.as_view(), name="create_task"),
+    #http://localhost:8000/task/create/
+    path(route="create/", view=TaskCreateView.as_view(), name="task_create"),
+    #http://localhost:8000/task/update/
+    path(route="update/<int:pk>/", view=TaskUpdateView.as_view(), name="task_update"),
 ]
--- backend/task/views.py
+++ backend/task/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import TaskModel
 
@@ -25,3 +25,11 @@ class TaskCreateView(CreateView):
     model = TaskModel
     fields = "__all__"
     success_url = reverse_lazy("tasks")
+
+
+class TaskUpdateView(UpdateView):
+    """This class used for modify specific task"""
+
+    model = TaskModel
+    fields = "__all__"
+    success_url = reverse_lazy("task")
