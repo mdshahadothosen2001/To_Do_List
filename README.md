6a26cf6ebeada482096cd480c2a1c98c074e989b
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-12-21 08:11:58 +0600

    feat: user account register

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # To_Do_List
+
+This project is made to manage the daily tasks.
+
+Tech stack: Python, Django, Docker.
+
+
+## Local development installation
+1. At first pull the repository.
+
+
+2. Create a virtual environment then activate it.
+
+```bash
+py -m venv venv
+```
+```bash
+venv/Scripts/activate.bate
+```
+
+3. Install the required packages.
+
+```bash
+pip install -r backend/requirements/development.txt
+```
+
+
+4. Run docker containers:
+
+```bash
+docker-compose up --build
+```
+
+click here [URL](http://localhost:8000/task/)  for check this project working or not
+If you see login interface then it is working.
+
+## Access token and authentication
+
+1. http://localhost:8000/user/register/ api hit for user account register.
+2. http://localhost:8000/user/login/ api hit for user login.
+3. http://localhost:8000/task/ api hit for see task list.
+4. http://localhost:8000/task/1/ api hit for see task detail.
+5. http://localhost:8000/task/create/ api hit for create a new task.
+6. http://localhost:8000/task/update/1/ api hit for modify particular task.
+7. http://localhost:8000/task/delete/1/ api hit for task delete.
+9. http://localhost:8000/user/logout/ api hit for user logout.
--- backend/task/registrations/views.py
+++ backend/task/registrations/views.py
@@ -1,9 +1,38 @@
 from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.contrib.auth import logout
+from django.contrib.auth import login, logout
 from django.shortcuts import redirect
 from django.views import View
 
+
+
+class UserCreationView(FormView):
+    """This class used for user registration for task application"""
+
+    template_name = "registrations/user_register.html"
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy("tasks")
+
+    def form_valid(self, form):
+        """Used for check form data and save valid form"""
+
+        user = form.save()
+
+        if user is not None:
+            login(self.request, user)
+
+        return super(UserCreationView, self).form_valid(form)
+    
+    def get(self, request, *args, **kwargs):
+        """Used for check user id authenticated then redirect"""
+
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+
+        return super(UserCreationView, self).get(*args, **kwargs)
 
 
 class UserLoginView(LoginView):
